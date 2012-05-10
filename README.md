13ea876572e1319e687368828d109a9a861c2ea9
Jesse Bounds <jesse@rebounds.net>
2012-05-09 17:11:45 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+pen311 Analytics Proxy Server
+
+![Mou icon](http://codeforamerica.org/wp-content/themes/cfawp2012/images/logo.png)
+
+#### What is the meaning of this?
+
+Open311 Analytics is powered by a MongoHQ MongoDB. As such, there is a RESTful API provided by MongoHQ that allows for a more expressive means of querying Open311 data as compared to using a raw Open311 endpoint. This is because Open311 as a specification is focused more on request input by citizens and less on data reading and analysis.
+
+This proxy server is used to insulate the MongoHQ instance (and private account information) from users of the service.
+
+**This project is in beta and may change significantly in the future!**
+
+#### Setup and Deployment
+
+The proxy server is easy to run locally and deploy to Heroku. 
+
+##### Running locally
+
+Clone the project:
+
+	$ git clone https://github/clone/this
+
+Go to the proxy directory:
+
+	$ cd mongohq_proxy
+
+Run the application like this:
+
+	APIkey=your_mongohq_api_key node testSimple.js
+	
+**Note:** You can also set the APIkey environment variable by exporting it or, if you are using foreman, you can create a .env file.
+
+##### Deploy to Heroku
+
---
+++ proxy.js
@@ -0,0 +1,95 @@
+var http = require('http'),
+    https = require('https'),
+    util = require('util'),
+    colors = require('colors');
+
+var welcome = [
+  '#     #     #####  #####   ####  #    # #   #',
+  '##   ##     #    # #    # #    #  #  #   # # ',  
+  '# # # #     #    # #    # #    #   ##     #  ',   
+  '#  #  # ### #####  #####  #    #   ##     #  ',   
+  '#     #     #      #   #  #    #  #  #    #  ',   
+  '#     #     #      #    #  ####  #    #   #  '
+].join('\n');
+util.puts(welcome.rainbow.bold);
+
+console.log("started with APIkey: " + process.env.APIkey);
+
+//
+// Http Proxy Server
+//
+http.createServer(function (request, proxyResponse) {
+
+  console.log("recieved proxy request with path: " + request.url);
+  if (request.url === "/favicon.ico") {
+    proxyResponse.writeHead(404);
+    proxyResponse.end();
+    console.log(request.url + " " + 404);
+    return;
+  }
+  
+  // connecting to mongohq using secret key and path supplied by request
+  var options = {
+    host: 'api.mongohq.com',
+    port: 443,
+    path: request.url + "&_apikey=" + process.env.APIkey,
+    method: 'GET'
+  };
+
+  // make https call to mongohq
+  var req = https.request(options, function(res) {
+    
+    console.log("querying: " + options['path']);
+
+    // hold data we recieve from mongohq
+    var buffers = new Array();
+    
+    // when data is recieved, capture it
+    res.on('data', function(data) {
+      buffers.push(data);
+    });
+
+    // when call to proxy is complete, return the 
+    // as if we were mongohq
+    res.on('end', function() {
+      //console.log("statusCode: ", res.statusCode);
+      //console.log("headers: ", res.headers);
+      proxyResponse.writeHead(res.statusCode,
+                        {'Content-Type': res.headers['content-type'],
+                         'server': res.headers['server'],
+                         'date': res.headers['date'],
+                         'connection': res.headers['connection']});
+      allBuffers = null;
+      buffers.forEach(function(buffer) {
+        console.log(buffer.length);
+        if (allBuffers === null) {
+          allBuffers = buffer;
+        } else {
+          var currentLength = allBuffers.length;
+          console.log("currentLength = " + currentLength);
+          var newLength = currentLength + buffer.length;
+          console.log("newLength = " + newLength);
+          expandBuffer = new Buffer(newLength);
+          //expandBuffer.copy(allBuffers);
+          allBuffers.copy(expandBuffer);
+          console.log("expandBuffer is now " + expandBuffer.length);
+          console.log("currentLength = " + currentLength);
+          //expandBuffer.copy(buffer, 0, currentLength+1, newLength);
+          buffer.copy(expandBuffer, currentLength, 0);
+          allBuffers = expandBuffer;
+        }
+      });
+      proxyResponse.end(allBuffers.toString());
+    });
+  });
+  req.end();
+
+  // write errors to console
+  req.on('error', function(e) {
+    console.error(e);
+  });
+
+}).listen(3000, "localhost");
+
+util.puts('http proxy server'.blue + ' started '.green.bold + 'on port '.blue + '3000'.yellow);
+
